26bdfb9d5bb00c118ac8a4d06b45b1271ec8c59b
it <email@e.com>
2022-10-10 22:56:58 +0300

    init eurika-server

---
+++ spring-cloud-microservices-demo-modules/eureka-server/src/test/java/org/microserives/demo/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.microserives.demo.eureka.server;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class EurekaServerApplicationTests {
+
+    @Test
+    @Disabled
+    void contextLoads() {
+    }
+}
