a428b3d68a441cceea41972ade19928528b09cd8
it <email@e.com>
2022-10-14 22:19:21 +0300

    modify ApiRestController

--- spring-cloud-microservices-demo-modules/eureka-client/src/main/java/org/microserives/demo/eurekaclient/web/ApiRestController.java
+++ spring-cloud-microservices-demo-modules/eureka-client/src/main/java/org/microserives/demo/eurekaclient/web/ApiRestController.java
@@ -17,9 +17,6 @@ public class ApiRestController {
     @Value("${spring.application.name}")
     private String nameService;
 
-    @Value("${eureka.client.service-url.defaultZone}")
-    private String uriServiceDiscovery;
-
    // @Value("${eureka.instance.instanceId}")
     private String idService;
 
@@ -41,8 +38,6 @@ public class ApiRestController {
 
         return "Hello. It's a microservice (name): " +
                 nameService +
-                " --- Uri a Discovery Service : " +
-                uriServiceDiscovery +
                 " --- PID the application : " +
                 System.getProperty("PID") +
                 " --- Port the application : " +
