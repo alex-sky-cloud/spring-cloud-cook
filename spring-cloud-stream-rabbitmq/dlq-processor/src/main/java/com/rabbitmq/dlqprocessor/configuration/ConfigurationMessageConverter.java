66cf0ba79a43c69d54c9c138e0c725a438300877
it <email@e.com>
2022-11-24 13:29:17 +0300

    modify ConfigurationMessageConverter

--- spring-cloud-stream-rabbitmq/dlq-processor/src/main/java/com/rabbitmq/dlqprocessor/configuration/ConfigurationMessageConverter.java
+++ spring-cloud-stream-rabbitmq/dlq-processor/src/main/java/com/rabbitmq/dlqprocessor/configuration/ConfigurationMessageConverter.java
@@ -7,6 +7,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigurationMessageConverter {
 
+    /**
+     * Необходим, чтобы избежать ошибки, во время десериализации
+     * сообщений.
+     * Например, нужно  определить конфигурацию конвертации JSON,
+     * так как если сообщения публикуются в брокер в формате JSON,
+     * если именно такой формат сообщений мы указали, когда настраивали в service,
+     * который публикует сообщения в брокер
+     */
     @Bean
     public Jackson2JsonMessageConverter converter() {
         return new Jackson2JsonMessageConverter();
